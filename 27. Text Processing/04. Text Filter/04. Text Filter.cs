055956fc5fdaf6de5d755c6dc67861b7268082ed
Momchil Ivanov <88820552+Momchil-Ivanov@users.noreply.github.com>
2021-08-12 13:12:18 +0300

    Add files via upload

---
+++ 27. Text Processing/04. Text Filter/04. Text Filter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] censoredWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+            foreach (var word in censoredWords)
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ 27. Text Processing/05. Digits, Letters and Other/05. Digits, Letters and Other.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace _05._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder letters = new StringBuilder();
+            StringBuilder numbers = new StringBuilder();
+            StringBuilder specials = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    numbers.Append(input[i]);
+                }
+                else if (char.IsLetter(input[i]))
+                {
+                    letters.Append(input[i]);
+                }
+                else
+                {
+                    specials.Append(input[i]);
+                }
+            }
+
+            Console.WriteLine(numbers);
+            Console.WriteLine(letters);
+            Console.WriteLine(specials);
+        }
+    }
+}
