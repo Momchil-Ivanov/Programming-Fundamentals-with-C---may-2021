8d67166590de5be6c0480217b08279f239612d97
Momchil Ivanov <88820552+Momchil-Ivanov@users.noreply.github.com>
2021-08-12 12:59:48 +0300

    Add files via upload

---
+++ 20. Objects and Classes/02. Big Factorial/02. Big Factorial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02._Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            FactorialCalculator calculator = new FactorialCalculator(n);
+            Console.WriteLine(calculator.Calculate());
+        }
+    }
+    class FactorialCalculator
+    {
+        public FactorialCalculator(int n)
+        {
+            N = n;
+        }
+        public int N { get; set; }
+
+        public BigInteger Calculate()
+        {
+            BigInteger factorial = 1;
+            for (int i = 1; i <= N; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+
+}
---
+++ 20. Objects and Classes/06. Store Boxes/06. Store Boxes.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Vehicle_Catalogue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box> boxes = new List<Box>();
+
+            string line = Console.ReadLine();
+            while (line != "end")
+            {
+                string[] tokens = line.Split();
+
+                int serial = int.Parse(tokens[0]);
+                string name = tokens[1];
+                int quantity = int.Parse(tokens[2]);
+                double itemPrice = double.Parse(tokens[3]);
+                double totalPrice = itemPrice * quantity;
+
+                Box box = new Box()
+                {
+                    Serial = serial,
+                    Name = name,
+                    Quantity = quantity,
+                    ItemPrice = itemPrice,
+                    TotalPrice = totalPrice
+                };
+
+                boxes.Add(box);
+                line = Console.ReadLine();
+            }
+
+            boxes.Sort((s1, s2) => s2.TotalPrice.CompareTo(s1.TotalPrice));
+            foreach (Box box in boxes)
+            {
+
+                Console.WriteLine($"{box.Serial}");
+                Console.WriteLine($"-- {box.Name} - ${box.ItemPrice:f2}: {box.Quantity}");
+                Console.WriteLine($"-- ${box.TotalPrice:f2}");
+            }
+
+        }
+    }
+
+
+
+    class Box
+    {
+        public int Serial { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double ItemPrice { get; set; }
+
+        public double TotalPrice { get; set; }
+    }
+}
+
