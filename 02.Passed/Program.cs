279ee596455efc4e9891d2cb6e3229d2812d6d02
Momchil Ivanov <88820552+Momchil-Ivanov@users.noreply.github.com>
2021-08-12 11:27:16 +0300

    Add files via upload

---
+++ 02.Passed/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02.Passed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Take as an input a grade and check if the student has passed the exam (grade>=3.00)
+
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
