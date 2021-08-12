7b671987202ab81205358d8ffa4f8aa3fe92c096
Momchil Ivanov <88820552+Momchil-Ivanov@users.noreply.github.com>
2021-08-12 12:53:51 +0300

    Add files via upload

---
+++ 17. Lists/06. List Manipulation Basics/06. List Manipulation Basics.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _00._Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] commandArgs = command.Split();
+                string action = commandArgs[0];
+                int element = int.Parse(commandArgs[1]);
+
+                if (action == "Add")
+                {
+                    numbers.Add(element);
+                }
+
+                if (action == "Remove")
+                {
+                    numbers.Remove(element);
+                }
+
+                if (action == "RemoveAt")
+                {
+                    numbers.RemoveAt(element);
+                }
+
+                if (action == "Insert")
+                {
+                    int index = int.Parse(commandArgs[2]);
+                    numbers.Insert(index, element);
+
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ 17. Lists/07. List Manipulation Advanced/07. List Manipulation Advanced.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _00._Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> numbersOriginal = new List<int>(numbers);
+
+            string command = Console.ReadLine();
+
+            bool change = false;
+
+            while (command != "end")
+            {
+
+                string[] commandArgs = command.Split();
+                string action = commandArgs[0];
+
+                if (action == "Filter")
+                {
+                    string condition = commandArgs[1];
+
+                    int element = int.Parse(commandArgs[2]);
+
+                    if (condition == "<")
+                    {
+                        List<int> testedNumbers = new List<int>(numbers);
+                        testedNumbers.RemoveAll(n => n >= element);
+                        Console.WriteLine(string.Join(" ", testedNumbers));
+                    }
+
+                    if (condition == ">")
+                    {
+                        List<int> testedNumbers = new List<int>(numbers);
+                        testedNumbers.RemoveAll(n => n <= element);
+                        Console.WriteLine(string.Join(" ", testedNumbers));
+                    }
+
+                    if (condition == "<=")
+                    {
+                        List<int> testedNumbers = new List<int>(numbers);
+                        testedNumbers.RemoveAll(n => n > element);
+                        Console.WriteLine(string.Join(" ", testedNumbers));
+                    }
+
+                    if (condition == ">=")
+                    {
+                        List<int> testedNumbers = new List<int>(numbers);
+                        testedNumbers.RemoveAll(n => n < element);
+                        Console.WriteLine(string.Join(" ", testedNumbers));
+                    }
+
+                }
+
+                if (action == "Add")
+                {
+                    int element = int.Parse(commandArgs[1]);
+
+                    numbers.Add(element);
+                    change = true;
+                }
+
+                if (action == "Remove")
+                {
+                    int element = int.Parse(commandArgs[1]);
+
+                    numbers.Remove(element);
+
+                    change = true;
+
+                }
+
+                if (action == "RemoveAt")
+                {
+                    int element = int.Parse(commandArgs[1]);
+
+                    numbers.RemoveAt(element);
+
+                    change = true;
+
+                }
+
+                if (action == "Insert")
+                {
+                    int element = int.Parse(commandArgs[1]);
+
+                    int index = int.Parse(commandArgs[2]);
+                    numbers.Insert(index, element);
+
+                    change = true;
+
+                }
+
+                if (action == "Contains")
+                {
+                    int element = int.Parse(commandArgs[1]);
+
+                    numbers.Contains(element);
+                    if (numbers.Contains(element) == true)
+                    {
+                        Console.WriteLine($"Yes");
+                    }
+                    if (numbers.Contains(element) == false)
+                    {
+                        Console.WriteLine($"No such number");
+                    }
+                }
+
+                if (action == "PrintEven")
+                {
+                    List<int> testedNumbers = new List<int>(numbers);
+                    testedNumbers.RemoveAll(n => n % 2 != 0);
+                    Console.WriteLine(string.Join(" ", testedNumbers));
+
+                }
+
+                if (action == "PrintOdd")
+                {
+                    List<int> testedNumbers = new List<int>(numbers);
+                    testedNumbers.RemoveAll(n => n % 2 == 0);
+                    Console.WriteLine(string.Join(" ", testedNumbers));
+
+                }
+
+                if (action == "GetSum")
+                {
+                    List<int> testedNumbers = new List<int>(numbers);
+                    Console.WriteLine(testedNumbers.Sum());
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+            if (change)
+
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+
+            }
+
+        }
+    }
+}
